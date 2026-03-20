ed764a5708ef09bf0bb61383a399c2d0fe625188
timothyngcy <timothyngcongyao@gmail.com>
2026-03-20 17:31:14 +0800

    feat: implement function to fetch all planning areas over the years

--- onemap_ingest/ingest_onemap_api.py
+++ onemap_ingest/ingest_onemap_api.py
@@ -18,3 +18,26 @@ if not AUTH_RESPONSE.ok:
 AUTH_DATA = AUTH_RESPONSE.json()
 ACCESS_TOKEN = AUTH_DATA.get("access_token")
 HEADERS = {"Authorization": ACCESS_TOKEN}
+
+PLANNING_AREA_URL = "https://www.onemap.gov.sg/api/public/popapi/getPlanningareaNames"
+
+def get_planning_areas():
+    '''
+    Fetches planning areas over the years from the OneMap API.
+    '''
+    years = [1998, 2008, 2014, 2019]
+
+    all_data = []
+
+    for year in years:
+        params = {"year": year}
+        response = requests.get(PLANNING_AREA_URL, params=params, headers=HEADERS, timeout=15)
+        data = response.json()
+
+        for planning_area in data['SearchResults']:
+            all_data.append({
+                "planning_area": planning_area['pln_area_n'],
+                "year": year
+            })
+
+    return all_data
